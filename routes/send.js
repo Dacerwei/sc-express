6db52c0cbfe46fe60b4c00b65b3291a1ebba0545
Chinwei.Hsu <dacerwei@gmail.com>
2017-01-06 18:26:02 +0800

    fromWei dtat

--- routes/send.js
+++ routes/send.js
@@ -57,7 +57,7 @@ router.get('/', function(req, res, next) {
 	]);
 
 	var theDeal = dealContract.at(smartContractAddress);
-	theDeal.pay.sendTransaction(web3.toWei(amount,"ether"),{from:payerAddress, value:web3.toWei(amount,"ether")});
+	theDeal.pay.sendTransaction(web3.toWei(amount,"ether"),{from:payerAddress, value:web3.toWei(amount+3,"ether")});
 	res.json({
 		"state": true
 	});
